eb920348f4f876abb64e363be68675bfdb21cec9
shinshin86 <beagles1986@gmail.com>
2024-09-29 10:39:04 +0900

    fix problem of COOP/COEP

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,21 @@
-import { defineConfig } from 'vite';
+import { defineConfig, Plugin } from 'vite';
 import react from '@vitejs/plugin-react';
+
+const htmlPlugin = (): Plugin => {
+  return {
+    name: 'html-transform',
+    transformIndexHtml(html: string) {
+      return html.replace(
+        '</title>',
+        `</title>\n    <script src="/mytodolist/assets/coi-serviceworker.js"></script>`,
+      );
+    },
+  };
+};
 
 export default defineConfig({
   base: '/mytodolist/',
-  plugins: [react()],
+  plugins: [react(), htmlPlugin()],
   server: {
     headers: {
       'Cross-Origin-Opener-Policy': 'same-origin',
